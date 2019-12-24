a6bc5c562aad4901e70d32d11811bb9937601a36
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-12-24 12:38:36 -0600

    complete refactor to basic site

--- audrey-marcum/src/App.js
+++ audrey-marcum/src/App.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Homepage from "./components/Homepage";
-import Book from "./components/Book";
 
 function App() {
-  const [currentPage, setPage] = useState("home");
-
-  const bookPage = () => {
-    setPage("book");
-  };
-
   return (
     <div className="App">
       <Sidebar />
-      {currentPage === "home" && <Homepage bookPage={bookPage} />}
-      {currentPage === "book" && <Book />}
+      <Homepage />
     </div>
   );
 }
